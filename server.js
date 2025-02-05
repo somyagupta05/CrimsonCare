ea1482a5d59e72af661e94b166c1e3a48929e512
somyagupta05 <somyagupta052003@gmail.com>
2025-02-06 01:06:51 +0530

    setup server

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+dotenv.config();
 const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 // test routes
 app.use("/api/v1/test", require("./routes/testRoutes"));
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log("node server running");
+  console.log(
+    `server is runing in ${process.env.DEV_MODE} Modeon Por ${process.env.PORT}`
+      .bgBlue.white
+  );
 });
