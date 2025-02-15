f79f709434de7f31894bb1d6b905616de1a1a9d0
somyagupta05 <somyagupta052003@gmail.com>
2025-02-15 11:04:53 +0530

    user model

---
+++ models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    required: [true, "role is required"],
+    enum: ["admin", "organization", "user", "hospital"],
+  },
+  name: {
+    type: String,
+    required: function () {
+      if (this.role === "user" || this.role === "admin") {
+        return true;
+      }
+      return false;
+    },
+  },
+  organisationName: {
+    type: String,
+    required: function () {
+      if (this.role === "organisation") {
+        return true;
+      }
+      return false;
+    },
+  },
+
+  email: {
+    type: String,
+    require: [true, "email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  website: {
+    type: String,
+  },
+  address: {
+    type: String,
+    required: [true, "address is required"],
+  },
+  phone: {
+    type: String,
+    required: [true, "phone number is required"],
+  },
+});
