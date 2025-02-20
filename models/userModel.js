6c17b65411183a95c8dd3827aff5b8f745b9fca6
somyagupta05 <somyagupta052003@gmail.com>
2025-02-21 00:09:15 +0530

    get current user

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       required: [true, "role is required"],
-      enum: ["admin", "organization", "user", "hospital"],
+      enum: ["admin", "organization", "donar", "hospital"],
     },
     name: {
       type: String,
