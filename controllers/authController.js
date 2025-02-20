87ea9d3b62937d52c2de822e70a4bea0c6bb1181
somyagupta05 <somyagupta052003@gmail.com>
2025-02-20 23:57:51 +0530

    get current user

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,6 +73,22 @@ const loginContoller = async (req, res) => {
 };
 
 // get current user
-const currentUserController = () => {};
+const currentUserController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ _id: req.body.userId });
+    return res.status(200).send({
+      success: true,
+      message: "user fetched successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "unable to get current user",
+      error,
+    });
+  }
+};
 
 module.exports = { registerController, loginContoller, currentUserController };
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ module.exports = async (req, res, next) => {
           message: "Auth failed",
         });
       } else {
-        req.body.userId = decode.id;
+        req.body.userId = decode.userId;
         next();
       }
     });
