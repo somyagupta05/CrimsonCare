8c7d6087f0e3308bca38a411bbd7b81951d254c8
somyagupta05 <somyagupta052003@gmail.com>
2025-02-22 23:42:19 +0530

    fixed routes of login register

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div><h1>Login</h1></div>
-  )
-}
+    <>
+      <div className="row">
+        <div className="col-md-8 form-banner">
+          <img src="./assests/images/banner1.jpg" />
+        </div>
+        <div className="col-md-4 form-container">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+            
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
 
-export default Login
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
