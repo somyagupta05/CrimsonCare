3a4385b56b14991d878e8a054f0720da837b9fa7
somyagupta05 <somyagupta052003@gmail.com>
2025-02-23 00:27:59 +0530

    creating login and register with all fields

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,23 +1,17 @@
 import React from "react";
-import InputType from "../../components/Shared/Form/InputType";
+import Form from "../../components/Shared/Form/Form";
+// import InputType from "../../components/Shared/Form/InputType";
 // import { name } from './../../../node_modules/jiti/dist/babel';
 
 const Login = () => {
   return (
     <>
-      <div className="row">
+      <div className="row g-0">
         <div className="col-md-8 form-banner">
           <img src="./assests/images/banner1.jpg" />
         </div>
         <div className="col-md-4 form-container">
-          <form>
-          <InputType labelText={'EMail Address'} labelFor={'forEMail'} inputType={'email'} name={"email"}/>
-           
-
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+          <Form formTitle={'Login Page'} submitBtn={'login'}/>
         </div>
       </div>
     </>
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Form from '../../components/Shared/Form/Form'
 const Register = () => {
   return (
-    <div><h1>Register</h1></div>
+   <>
+   <div className="row g-0">
+    <div className="col-md-8 form-banner">
+      <img src="./assests/images/banner2.jpg" alt="registerImage"/>
+      </div> 
+      <div className="col-md-4 form-container">
+        <Form formTitle={'Register'} submitBtn={'Register'}/>
+      </div>
+   </div>
+   </>
   )
 }
 
