090459dc2bfb9bcb62c6dff121855b161d8877e6
somyagupta05 <somyagupta052003@gmail.com>
2025-02-27 14:52:20 +0530

    auth service done

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,9 @@
 export const handleLogin=(e,email,password,role)=>{
   e.preventDefault()
   try{
+    if(!role || !email || !password){
+      return alert('please provide all fields')
+    }
     console.log('login',e,email,password,role);
   }
   catch(error){
