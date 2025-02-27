8e99db578ffc07af7f1b07fd59fce4fc45f07290
somyagupta05 <somyagupta052003@gmail.com>
2025-02-27 14:47:59 +0530

    form

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,41 @@
-export const handleLogin=()=>{
+export const handleLogin=(e,email,password,role)=>{
+  e.preventDefault()
+  try{
+    console.log('login',e,email,password,role);
+  }
+  catch(error){
+    console.log(error);
+  }
   
 }
 
-export const handleRegister=()=>{}
+export const handleRegister=(  e,
+  name,
+  role,
+  email,
+  password,
+  organisationName,
+  hospitalName,
+  website,
+  address,
+  phone)=>{
+    e.preventDefault();
+    try{
+      console.log(
+      'Register =>',
+      e,
+      name,
+      role,
+      email,
+      password,
+      organisationName,
+      hospitalName,
+      website,
+      address,
+      phone);
+    }
+    catch(error){
+      console.log(error)
+    }
+
+}
