e938a56c2a7685cf20667bd57a6b0fb5212a4cb6
somyagupta05 <somyagupta052003@gmail.com>
2025-02-28 15:16:16 +0530

    done with register action and reducer

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,47 +1,47 @@
-import { userLogin } from '../redux/features/auth/authAction'
-import store from '../redux/store'
+// import { userLogin, userRegister } from "../redux/features/auth/authAction";
+import { userLogin,userRegister } from "../redux/features/auth/authAction";
+import store from "../redux/store";
 
-export const handleLogin=(e,email,password,role)=>{
-  e.preventDefault()
-  try{
-    if(!role || !email || !password){
-      return alert('please provide all fields')
+export const handleLogin = (e, email, password, role) => {
+  e.preventDefault();
+  try {
+    if (!role || !email || !password) {
+      return alert("Please Privde All Feilds");
     }
-   store.dispatch(userLogin({email,password,role}))
-  }
-  catch(error){
+    store.dispatch(userLogin({ email, password, role }));
+  } catch (error) {
     console.log(error);
   }
-  
-}
+};
 
-export const handleRegister=(  e,
+export const handleRegister = (
+  e,
   name,
   role,
   email,
   password,
+  phone,
   organisationName,
-  hospitalName,
-  website,
   address,
-  phone)=>{
-    e.preventDefault();
-    try{
-      console.log(
-      'Register =>',
-      e,
-      name,
-      role,
-      email,
-      password,
-      organisationName,
-      hospitalName,
-      website,
-      address,
-      phone);
-    }
-    catch(error){
-      console.log(error)
-    }
-
-}
+  hospitalName,
+  website
+) => {
+  e.preventDefault();
+  try {
+    store.dispatch(
+      userRegister({
+        name,
+        role,
+        email,
+        password,
+        phone,
+        organisationName,
+        address,
+        hospitalName,
+        website,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -52,7 +52,7 @@ const userSchema = new mongoose.Schema(
     },
     phone: {
       type: String,
-      required: [true, "phone numbe is required"],
+      required: [false, "phone numbe is required"],
     },
   },
   { timestamps: true }
