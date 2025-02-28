98dc84fc24d6311a759f79fc86e6b048b99b4ad5
somyagupta05 <somyagupta052003@gmail.com>
2025-02-28 14:53:57 +0530

    Axios Interceptor

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,10 +1,13 @@
+import { userLogin } from '../redux/features/auth/authAction'
+import store from '../redux/store'
+
 export const handleLogin=(e,email,password,role)=>{
   e.preventDefault()
   try{
     if(!role || !email || !password){
       return alert('please provide all fields')
     }
-    console.log('login',e,email,password,role);
+   store.dispatch(userLogin({email,password,role}))
   }
   catch(error){
     console.log(error);
