6c133035a65557b2776e2ca4712fea1191ce9a7f
somyagupta05 <somyagupta052003@gmail.com>
2025-03-04 22:46:01 +0530

    public and protected route

--- client/src/components/Routes/ProtectedRoute.js
+++ client/src/components/Routes/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import API from "../../services/API";
-import { getCurrentUser } from "../../redux/features/auth/authActions";
+import { getCurrentUser } from "../../redux/features/auth/authAction";
 import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const dispatch = useDispatch();
